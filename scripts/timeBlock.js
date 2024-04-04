c533ae1173286fe4862e6c0383e4755da9ffda07
ivanmirable <ivankrivonogov1823@mail.ru>
2024-04-04 18:31:29 +0400

    debugging-3-done-2

--- scripts/timeBlock.js
+++ scripts/timeBlock.js
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let html = '';
     const timeData = data === '24_hours' ? nearestTimeData : subsequentTimeData;
     const minWidth = data === '24_hours' ? '156px' : '275px'; // Установка различной ширины в зависимости от значения data
+
     timeData.forEach((item) => {
         html += `
           <div class="slider__content-item" style="min-width: ${minWidth};">
@@ -32,7 +33,11 @@ document.addEventListener("DOMContentLoaded", function () {
             <div class="slider__content-el">${item.temp}</div>
           </div>
        `;
-    });      
+    });    
+    html+=`    <button class="btn card forecast-slider__navigation-btn" disabled>
+    <img src="/7db0246f-d52/public/icons/arrowLeft.svg" alt="Назад">
+  </button>`  
+
     return html;
   }
 
